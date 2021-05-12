ec10bd4d3f767a7e08d8e177d749619c967cab21
sylvianikol <silviaadvert@gmail.com>
2021-05-12 09:45:13 +0300

    Initial commit

---
+++ src/com/syn/chapter2/item1_staticfactorymethod/User.java
@@ -0,0 +1,29 @@
+package com.syn.chapter2.item1_staticfactorymethod;
+
+import java.time.LocalTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class User {
+
+    private static final Logger LOGGER = Logger.getLogger(User.class.getName());
+    private static final String DEFAULT_COUNTRY = "Default Country";
+    private String name;
+    private String email;
+    private String country;
+
+    public User(String name, String email, String country) {
+        this.name = name;
+        this.email = email;
+        this.country = country;
+    }
+
+    public static User createWithDefaultCountry(String name, String email) {
+        return new User(name, email, DEFAULT_COUNTRY);
+    }
+
+    public static User createWithLoggedInstantiationTime(String name, String email, String country) {
+        LOGGER.log(Level.INFO, "User created at : {0}", LocalTime.now());
+        return new User(name, email, country);
+    }
+}
