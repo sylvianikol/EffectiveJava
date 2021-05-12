17cc19153a66d5bb527289f1a4ecc54f4187840f
sylvianikol <silviaadvert@gmail.com>
2021-05-12 09:54:11 +0300

    implemented an instance-controlled static factory method

--- src/com/syn/chapter2/item1_staticfactorymethod/User.java
+++ src/com/syn/chapter2/item1_staticfactorymethod/User.java
@@ -8,6 +8,9 @@ public class User {
 
     private static final Logger LOGGER = Logger.getLogger(User.class.getName());
     private static final String DEFAULT_COUNTRY = "Default Country";
+
+    private static volatile User instance = null;
+
     private String name;
     private String email;
     private String country;
@@ -16,6 +19,18 @@ public class User {
         this.name = name;
         this.email = email;
         this.country = country;
+    }
+
+    public static User getInstance(String name, String email, String country) {
+        if (instance == null) {
+            synchronized (User.class) {
+                if (instance == null) {
+                    instance = new User(name, email, country);
+                }
+            }
+        }
+
+        return instance;
     }
 
     public static User createWithDefaultCountry(String name, String email) {
