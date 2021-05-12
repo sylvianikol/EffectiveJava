653f436d28dbe910f652de50271aa5493e9bfb7a
sylvianikol <silviaadvert@gmail.com>
2021-05-12 15:23:18 +0300

    implemented a Builder pattern

---
+++ src/com/syn/item2_builder/MainApp.java
@@ -0,0 +1,20 @@
+package com.syn.item2_builder;
+
+
+import static com.syn.item2_builder.NYPizza.Size.SMALL;
+import static com.syn.item2_builder.Pizza.Topping.*;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+
+        NYPizza pizza = new NYPizza.Builder(SMALL)
+                .addTopping(SAUSAGE).addTopping(ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(HAM).sauceInside().build();
+    }
+}
