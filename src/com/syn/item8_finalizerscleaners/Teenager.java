2077c7e7afad7d499dbebe99556b193d926eb877
sylvianikol <silviaadvert@gmail.com>
2021-05-13 13:22:56 +0300

    implemented cleaner example

---
+++ src/com/syn/item8_finalizerscleaners/Teenager.java
@@ -0,0 +1,11 @@
+package com.syn.item8_finalizerscleaners;
+
+import com.syn.item8_finalizerscleaners.cleaner.Room;
+
+public class Teenager {
+    public static void main(String[] args) {
+
+        new Room(99);
+        System.out.println("Peace out");
+    }
+}
--- src/com/syn/item8_finalizerscleaners/cleaner/Adult.java
+++ src/com/syn/item8_finalizerscleaners/cleaner/Adult.java
@@ -1,12 +1,10 @@
 package com.syn.item8_finalizerscleaners.cleaner;
 
-public class RoomApp {
+public class Adult {
     public static void main(String[] args) {
 
-        Room room = new Room(8);
-
-        try {
-            room.close();
+        try (Room room = new Room(8)) {
+            System.out.println("Goodbye");
         } catch (Exception e) {
             e.printStackTrace();
         }
