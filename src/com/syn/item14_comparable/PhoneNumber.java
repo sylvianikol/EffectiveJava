6406f446f99f72688ceeba6881c2b5347ce4f407
sylvianikol <silviaadvert@gmail.com>
2021-05-15 08:43:01 +0300

    implemented Comparable with comparator construction methods

--- src/com/syn/item14_comparable/PhoneNumber.java
+++ src/com/syn/item14_comparable/PhoneNumber.java
@@ -1,6 +1,15 @@
 package com.syn.item14_comparable;
 
+import java.util.Comparator;
+
+import static java.util.Comparator.comparingInt;
+
 public class PhoneNumber implements Comparable<PhoneNumber> {
+
+    private static final Comparator<PhoneNumber> COMPARATOR =
+            comparingInt((PhoneNumber pn) -> pn.areaCode)
+                    .thenComparingInt(pn -> pn.prefix)
+                    .thenComparingInt(pn -> pn.lineNum);
 
     private final short areaCode;
     private final short prefix;
@@ -22,6 +31,8 @@ public class PhoneNumber implements Comparable<PhoneNumber> {
 
     @Override
     public int compareTo(PhoneNumber phoneNumber) {
+        // Multiple-field Comparable with primitive fields
+
         int result = Short.compare(areaCode, phoneNumber.areaCode);
 
         if (result == 0) {
@@ -31,6 +42,11 @@ public class PhoneNumber implements Comparable<PhoneNumber> {
             }
         }
 
-        return result;
+//        return result;
+
+        // Comparable with comparator construction methods
+        return COMPARATOR.compare(this, phoneNumber);
     }
+
+
 }
