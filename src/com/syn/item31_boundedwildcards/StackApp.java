7f701e74b9bc1305e35aea8830dba38d5e52a652
sylvianikol <silviaadvert@gmail.com>
2021-05-18 09:19:33 +0300

    implemented example of bounded wildcards

---
+++ src/com/syn/item31_boundedwildcards/StackApp.java
@@ -0,0 +1,25 @@
+package com.syn.item31_boundedwildcards;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StackApp {
+
+    public static void main(String[] args) {
+
+        Stack<Number> stack = new Stack<>();
+        Iterable<Integer> integers = List.of( 1, 2, 3 );
+
+        stack.pushAll(integers);
+
+//        while (!stack.isEmpty()) {
+//            System.out.println(stack.pop());
+//        }
+
+        Collection<Object> collection = new ArrayList<>();
+        stack.popAll(collection);
+
+        collection.forEach(System.out::println);
+    }
+}
