ba122588718f49f552bb4e178f612388ad51cf1d
syn <silviaadvert@gmail.com>
2021-05-18 11:03:11 +0300

    implemented examples of bounded wildcards

--- src/com/syn/item31_boundedwildcards/set/Utilities.java
+++ src/com/syn/item31_boundedwildcards/set/Utilities.java
@@ -24,4 +24,12 @@ public class Utilities {
 
         return result;
     }
+
+    public static void swap(List<?> list, int i, int j) {
+        swapHelper(list, i, j);
+    }
+
+    private static <E> void swapHelper(List<E> list, int i, int j) {
+        list.set(i, list.set(j, list.get(i)));
+    }
 }
