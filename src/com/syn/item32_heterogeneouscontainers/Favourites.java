22ce7b9127c00bd3a9bd9ae736c7c3edbc0c7fb5
syn <silviaadvert@gmail.com>
2021-05-18 11:48:50 +0300

    implemented examples of typesafe heterogeneous container

--- src/com/syn/item32_heterogeneouscontainers/Favourites.java
+++ src/com/syn/item32_heterogeneouscontainers/Favourites.java
@@ -2,14 +2,13 @@ package com.syn.item32_heterogeneouscontainers;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class Favourites {
 
     private Map<Class<?>, Object> favourites = new HashMap<>();
 
     public <T> void putFavourite(Class<T> type, T instance) {
-        favourites.put(Objects.requireNonNull(type), instance);
+        favourites.put(type, type.cast(instance));
     }
 
     public <T> T getFavourite(Class<T> type) {
