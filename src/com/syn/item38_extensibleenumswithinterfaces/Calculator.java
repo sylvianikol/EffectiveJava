a02227159e603822a2672c2edc89e77985bd427b
sylvianikol <silviaadvert@gmail.com>
2021-05-19 09:27:23 +0300

    implemented an example of extensible enums

--- src/com/syn/item38_extensibleenumswithinterfaces/Calculator.java
+++ src/com/syn/item38_extensibleenumswithinterfaces/Calculator.java
@@ -7,10 +7,18 @@ public class Calculator {
         double x = 2.0;
         double y = 4.0;
 
+        // 1st approach
         calculate(ExtendedOperation.class, x, y);
+
+        // 2nd approach
+        // calculate(Arrays.asList(ExtendedOperation.values()), x, y);
     }
 
-    public static <T extends Enum<T> & Operation> void calculate(Class<T> opEnumType, double x, double y) {
+    public static <T extends Enum<T> & Operation> void calculate(
+            // 1st approach
+            Class<T> opEnumType,
+            // 2nd approach Collection<? extends Operation> opSet
+            double x, double y) {
 
         for (Operation operation : opEnumType.getEnumConstants()) {
             System.out.printf("%f %s %f = %f%n",
