0324609b9cf3edb17561b137bd98af1c9cef2813
sylvianikol <silviaadvert@gmail.com>
2021-05-19 10:10:50 +0300

    implemented an example of Test annotations

--- src/com/syn/item39_annotations/RunTests.java
+++ src/com/syn/item39_annotations/RunTests.java
@@ -10,23 +10,47 @@ public class RunTests {
         int tests = 0;
         int passed = 0;
 
-        Class<?> testClass = Class.forName("com.syn.item39_annotations.Sample");
+        Class<?> testSample = Class.forName("com.syn.item39_annotations.Sample");
+        Class<?> testSample2 = Class.forName("com.syn.item39_annotations.Sample2");
 
-        for (Method method : testClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Test.class)) {
+        for (Method method : testSample2.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(ExceptionTest.class)) {
                 tests++;
-
                 try {
                     method.invoke(null);
-                    passed++;
-                } catch (InvocationTargetException wrappedExc) {
-                    Throwable ex = wrappedExc.getCause();
-                    System.out.println(method + " failed: " + ex);
-                } catch (Exception ex) {
+                    System.out.printf("Test %s failed: no exception%n", method);
+                } catch (InvocationTargetException wrappedEx) {
+                    Throwable exc = wrappedEx.getCause();
+                    Class<? extends Throwable> excType =
+                            method.getAnnotation(ExceptionTest.class).value();
+                    if (excType.isInstance(exc)) {
+                        passed++;
+                    } else {
+                        System.out.printf(
+                                "Test %s failed: expected %s, got %s%n",
+                                method, excType.getName(), exc);
+                    }
+                } catch (Exception exc) {
                     System.out.println("Invalid @Test: " + method);
                 }
             }
         }
+
+//        for (Method method : testSample.getDeclaredMethods()) {
+//            if (method.isAnnotationPresent(Test.class)) {
+//                tests++;
+//
+//                try {
+//                    method.invoke(null);
+//                    passed++;
+//                } catch (InvocationTargetException wrappedExc) {
+//                    Throwable ex = wrappedExc.getCause();
+//                    System.out.println(method + " failed: " + ex);
+//                } catch (Exception ex) {
+//                    System.out.println("Invalid @Test: " + method);
+//                }
+//            }
+//        }
 
         System.out.printf("Passed: %d, Failed: %d%n", passed, tests - passed);
     }
---
+++ src/com/syn/item39_annotations/Sample2.java
@@ -0,0 +1,17 @@
+package com.syn.item39_annotations;
+
+public class Sample2 {
+
+    @ExceptionTest(ArithmeticException.class)
+    public static void m1() { // Test should pass
+        int i = 0;
+        i = i / i;
+    }
+    @ExceptionTest(ArithmeticException.class)
+    public static void m2() { // Should fail (wrong exception)
+        int[] a = new int[0];
+        int i = a[1];
+    }
+    @ExceptionTest(ArithmeticException.class)
+    public static void m3() { } // Should fail (no exception)
+}
