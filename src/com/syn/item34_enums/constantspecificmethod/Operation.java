52ce98a64b175757cf61b7b3f2e5439af7155789
sylvianikol <silviaadvert@gmail.com>
2021-05-19 07:28:10 +0300

    implemented example of enum constant-specific method

--- src/com/syn/item34_enums/constantspecificmethod/Operation.java
+++ src/com/syn/item34_enums/constantspecificmethod/Operation.java
@@ -2,10 +2,29 @@ package com.syn.item34_enums.constantspecificmethod;
 
 public enum Operation {
 
-    PLUS { public double apply(double x, double y) { return x + y; }},
-    MINUS { public  double apply(double x, double y) { return x - y; }},
-    TIMES { public double apply(double x, double y) { return x * y; }},
-    DIVIDE { public double apply(double x, double y) { return x / y; }};
+    PLUS("+") {
+        public double apply(double x, double y) { return x + y; }
+        },
+    MINUS("-") {
+        public  double apply(double x, double y) { return x - y; }
+        },
+    TIMES("*") {
+        public double apply(double x, double y) { return x * y; }
+        },
+    DIVIDE("/") {
+        public double apply(double x, double y) { return x / y; }
+    };
+
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
 
     public abstract double apply(double x, double y);
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
 }
